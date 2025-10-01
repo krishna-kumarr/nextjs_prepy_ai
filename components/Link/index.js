7697835c0d6475d58af8cffcb70d75b5b0e57d8b
krishna-kumarr <kumarkrishna11231@gmail.com>
2025-10-01 15:38:40 +0530

    FIX: Link component

--- components/Link/index.js
+++ components/Link/index.js
@@ -1,5 +1,5 @@
 import Link from "next/link"
 
-export default function Link(props) {
+export default function LinkComponent(props) {
     return <Link href={props?.href || ''}>{props.title || ''}</Link>
 }
